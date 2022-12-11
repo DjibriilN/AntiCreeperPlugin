4e483603f0ac44a820398e82e505417b00b653ab
Djibril <Djibi@nefzi.net>
2022-12-11 21:07:08 +0100

    Changed name of method to something more understandable.

--- src/main/java/djibril/anticreeper/Creeperexplosion.java
+++ src/main/java/djibril/anticreeper/Creeperexplosion.java
@@ -1,17 +1,13 @@
 package djibril.anticreeper;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class Creeperexplosion implements Listener {
     @EventHandler
-    public void bumm(EntityExplodeEvent event){
+    public void Explosion(EntityExplodeEvent event){
         if(!(event.getEntity() instanceof Creeper))
             return;
         if(event.getEntity() instanceof Creeper)
